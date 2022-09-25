907686a920cf8b632a8c8adaec05f833045871b9
Dmytro Doroshkevych <ddoroshkevych@hotmail.com>
2022-09-24 23:16:08 -0400

    everything is in typescript

--- src/__tests__/pages/Hangman.test.tsx
+++ src/__tests__/pages/Hangman.test.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Hangman from '../../pages/Hangman';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Hangman />);
+  const linkElement = screen.getByText(/Hangman/i);
   expect(linkElement).toBeInTheDocument();
 });
